97d48beb6b22b6617a0f97d5d4c429d939cf2a2d
Siddharth Kale <kalesiddharthv@gmail.com>
2021-10-29 20:16:45 +0530

    used insertAdjacentHTML and added reset_setup

--- js/redirect.js
+++ js/redirect.js
@@ -3,50 +3,61 @@ const MEET_COMP = "Google";
 
 const now = new Date();
 const msg_box = document.getElementById("message");
+msg_box.innerHTML = "";
+
+if(localStorage && !localStorage.getItem("Setup")) {
+	localStorage.setItem("Setup", "0");
+}
 
 const day = now.getDay(), hour = now.getHours(), mins = now.getMinutes();
+function reset_account() {
+	if(!localStorage) {
+		msg_box.innerHTML = "Your broser does not support this app.";
+		return;
+	}
+	localStorage.setItem("Batch",prompt("This is one time setup.\nEnter your batch number.","1"));
+	localStorage.setItem("authuser",prompt("This is one time setup.\nEnter the serial number (index) of your college account on the list of all " + MEET_COMP + " accounts. (Leave 0 if on mobile device)","0"));
+	localStorage.setItem("Setup","1");
+	return;
+}
 function join() {
 	if(!localStorage) {
 		msg_box.innerHTML = "Your broser does not support this app.";
 		return;
 	}
-	if(!localStorage.getItem("Batch")) {
-		localStorage.setItem("Batch",prompt("This is one time setup.\nEnter your batch number.","1"));
-	}
-	if(!localStorage.getItem("authuser")) {
-		localStorage.setItem("authuser",prompt("This is one time setup.\nEnter the serial number (index) of your college account on the list of all " + MEET_COMP + "accounts. (Leave 0 if on mobile device)","0"));
+	if(localStorage.getItem("Setup") == "0") {
+		reset_account();
 	}
 	if(day == 0) {
-		msg_box.innerHTML = "It's sunday!! Sleep &#128564";
+		msg_box.insertAdjacentHTML("beforeend", "It's sunday!! Sleep &#128564");
 		return;
 	}
 	else {
-		Promise
-			.all([
-				fetch("../tt.json").then(res => res.json()),
-				fetch("../courses.json").then(res => res.json())
-			])
-			.then(res => {
-				let next_class_timing = 24;
-				for(let i of res[0][day - 1]) {
-					if(!i.Batch || i.Batch == localStorage.getItem("Batch")) {
-						if(hour >= i.StartTime && hour - i.StartTime < i.Duration) {
-							msg_box.innerHTML = "Joining lecture of " + i.CourseCode;
-							location.href = PREFIX_LINK + res[1][i.CourseCode]["GMeetCode"] + "?authuser=" + localStorage.getItem("authuser");
-							return;
-						}
-						if(hour < i.StartTime) {
-							next_class_timing = Math.min(next_class_timing, i.StartTime);
-						}
+		Promise.all([
+			fetch("../tt.json").then(res => res.json()),
+			fetch("../courses.json").then(res => res.json())
+		])
+		.then(res => {
+			let next_class_timing = 24;
+			for(let i of res[0][day - 1]) {
+				if(!i.Batch || i.Batch == localStorage.getItem("Batch")) {
+					if(hour >= i.StartTime && hour - i.StartTime < i.Duration) {
+						msg_box.insertAdjacentHTML("befoeend", "Joining lecture of " + i.CourseCode);
+						location.href = PREFIX_LINK + res[1][i.CourseCode]["GMeetCode"] + "?authuser=" + localStorage.getItem("authuser");
+						return;
+					}
+					if(hour < i.StartTime) {
+						next_class_timing = Math.min(next_class_timing, i.StartTime);
 					}
 				}
-				msg_box.innerHTML = "No class scheduled for now!";
-				if(next_class_timing == 24)
-					msg_box.innerHTML += "<br>That's it for the day";
-				else
-					alert("Next class at " + next_class_timing + ":00.");
-				return;
-			});
+			}
+			msg_box.insertAdjacentHTML("beforeend", "No class scheduled for now!");
+			if(next_class_timing == 24)
+				msg_box.insertAdjacentHTML("beforeend","<br>That's it for the day");
+			else
+				alert("Next class at " + next_class_timing + ":00.");
+			return;
+		});
 	}
 }
 
